7b811917b21b0eeaf82f34940122e912b2e6adfd
simenkrantz <simenkrantzknudsen@gmail.com>
2020-10-15 18:44:27 +0200

    Fixed show_board v1.0

--- chess.py
+++ chess.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 import numpy as np
 import math
-import enum
 
 class Board():
-    def __init__(self):
-        self.num_rows = 8
-        self.num_cols = 8
+    def __init__(self, num_rows = 8, num_cols = 8):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.board = np.ones(shape=(self.num_rows, self.num_cols))
 
         # TODO
-        #   Init board layout, 
+        #   Init board layout
+        #   
 
     def show_board(self):
-        print("Show chess board")
-
+        s = [[str(e) for e in row] for row in self.board]
+        lens = [max(map(len, col)) for col in zip(*s)]
+        fmt = ''.join('\t{{:{}}}'.format(x) for x in lens)
+        table = [fmt.format(*row) for row in s]
+        print '\n\n\n'.join(table)
 
 class Piece():
     def __init__(self, x_pos, y_pos, color):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.color = color # 'WHITE' or 'BLACK'
 
-    def move(self, new_x, new_y):
+    def move(self, board, new_x, new_y):
+        
         self.x_pos = new_x
         self.y_pos = new_y
-
 
 class Pawn(Piece):
     def __init__(self, x_pos, y_pos, color):
@@ -46,6 +49,11 @@ class Rook(Piece):
         super().__init__(x_pos, y_pos, color)
         self.has_moved = False
 
+    def move(self, new_x, new_y):
+        self.x_pos = new_x
+        self.y_pos = new_y
+        self.has_moved = True
+
 
 class Queen(Piece):
     def __init__(self, x_pos, y_pos, color):
@@ -57,6 +65,18 @@ class King(Piece):
         super().__init__(x_pos, y_pos, color)
         self.has_moved = False
         self.checked = False
+
+    def move(self, new_x, new_y):
+
+        # TODO
+        #   Check that the new position
+        #   isn't threatened by a piece of 
+        #   the opposite color
+
+        self.x_pos = new_x
+        self.y_pos = new_y
+        self.has_moved = True
+
 
 
 def check_castling(board, king, rook):
