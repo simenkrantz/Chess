6bfc9f26c0cc4b556f99d1b55215967f25c36487
simenkrantz <simenkrantzknudsen@gmail.com>
2020-10-15 18:08:11 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Chess project in Python
 
-
-
 ## Classes
 The following classes will be implemented:
 
@@ -9,17 +7,45 @@ The following classes will be implemented:
 8 x 8 matrix.
 
 Lower left corner is denoted a1 (black tile), upper right corner denoted h8.
+
 Row 1 is closest to the white player, row 8 is closest to the black player.
 
+### Piece
+#### Attributes
+Color: String 'WHITE' / 'BLACK'
+x_pos: Column
+y_pos: Row
 
 ### Pawn
+#### Attributes
+Inherits from `Piece`
 
 ### Knight
+#### Attributes
+Inherits from `Piece`
 
 ### Bishop
+#### Attributes
+Inherits from `Piece`
 
 ### Rook
+#### Attributes
+Inherits from `Piece`
+has_moved: Bool
 
 ### Queen
+#### Attributes
+Inherits from `Piece`
 
 ### King
+#### Attributes
+Inherits from `Piece`
+has_moved: Bool (default False)
+checked: Bool (default False)
+
+## Methods
+### `check_castling(board, king, rook)`
+Checks if castling, *rokade*, is possible.
+
+Returns boolean, `True` if neither king nor rook
+has moved, and the row between them is free of other pieces.
--- chess.py
+++ chess.py
@@ -1,13 +1,85 @@
 #!/usr/bin/python3
 import numpy as np
 import math
+import enum
 
 class Board():
     def __init__(self):
         self.num_rows = 8
         self.num_cols = 8
 
+        # TODO
+        #   Init board layout, 
+
+    def show_board(self):
+        print("Show chess board")
+
+
+class Piece():
+    def __init__(self, x_pos, y_pos, color):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.color = color # 'WHITE' or 'BLACK'
+
+    def move(self, new_x, new_y):
+        self.x_pos = new_x
+        self.y_pos = new_y
+
+
+class Pawn(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+
+
+class Knight(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+
+
+class Bishop(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+
+
+class Rook(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+        self.has_moved = False
+
+
+class Queen(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+
+
+class King(Piece):
+    def __init__(self, x_pos, y_pos, color):
+        super().__init__(x_pos, y_pos, color)
+        self.has_moved = False
+        self.checked = False
+
+
+def check_castling(board, king, rook):
+    """Check if a castling, O-O or O-O-O, is possible.
+
+    Parameter:
+        board:  Board object
+        king:   King object
+        rook:   Rook object
+    
+    @return: Bool
+    """
+    assert (king.color == rook.color), "Pieces are of different colors"
+    assert (not king.has_moved), "The king has moved"
+    assert (not rook.has_moved), "The rook has moved"
+
+    # TODO:
+    #   Loop over the board row and check that all tiles
+    #   are free for other pieces
+
+    pass
 
 
 if __name__ == "__main__":
-    pass
+    board = Board()
+    board.show_board()
