ce4955714b28e62ce9972b3347ddc042dd333995
simenkrantz <simenkrantzknudsen@gmail.com>
2020-10-18 21:48:45 +0200

    Able to get choice from terminal

--- run.py
+++ run.py
@@ -10,18 +10,25 @@ def run_game():
     board = Board()
     message_to_user = ""
     while True:
-        print(f"{bcolors.OKBLUE}Press 'Ctrl+C' or to exit the program{bcolors.ENDC}")
-        print("{}\n".format(message_to_user))
+        print(f"{bcolors.OKBLUE}Press Ctrl+C or to exit the program{bcolors.ENDC}")
+        print("Columns: a -- h\t\tRows: 1 -- 8")
+        print("{}".format(message_to_user))
         board.show_board()
 
+        piece_to_move = input("\nWhich piece do you want to move? ")
+        col = piece_to_move[0]      # a -- h
+        row = int(piece_to_move[1]) # 1 -- 8
+
+        if not isinstance(col, str) or not isinstance(row, int):
+            print("Invalid choice. Choose again")
+        else:
+            print("You want to move {}".format(piece_to_move))
+
         time.sleep(3)
+        
         os.system('clear')
 
-        # if piece.check_legal_move(x,y):
-        #   piece.move(x,y)
         
-
-
 
 if __name__ == "__main__":
     try:
